dc30310101a301cbf8d24346ff8c22992fd25991
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-01-10 21:07:09 +0545

    event propagation

--- learn some concepts/concept.js
+++ learn some concepts/concept.js
@@ -13,3 +13,27 @@ person.lastName = 'Doe';
 person.age = 50;
 
 console.table(person);
+
+
+// event bubbling in js
+// events are in 3 phases: catching, target, bubbling
+// eventcapturing: from Root to target
+// event bubbling: from target to root
+// event target: the element that triggered the event
+
+//example:
+// Parent element event listener
+document.getElementById("parent").addEventListener("click", function() {
+    console.log("Parent DIV Clicked");
+  }, true);  // Capturing phase (true)
+  
+  // Child element event listener
+  document.getElementById("child").addEventListener("click", function() {
+    console.log("Button Clicked");
+  });
+  
+  // Parent event listener in bubbling phase
+  document.getElementById("parent").addEventListener("click", function() {
+    console.log("Parent DIV Bubbling");
+  }, false);  // Bubbling phase (false)
+  
