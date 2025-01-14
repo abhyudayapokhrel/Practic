cb65bffdbc65801cdb9e1da9dc071c0ba940a027
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-01-14 14:44:22 +0545

    js on github finder

--- githubidfinder/app.js
+++ githubidfinder/app.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.github.com/users';
 const form = document.getElementById("form");
+const main = document.getElementById("main");
 const search = document.getElementById("search");
 
 getUser('abhyudayapokhrel');
@@ -7,13 +8,35 @@ getUser('abhyudayapokhrel');
 async function getUser(username){
     try{
         const {data} = await axios(APIURL + username);
-        console.log(data);
+        createUserCard(data);
 
     } catch(err){
         console.log(err);
     }
-
 }
+
+createUserCard = (user) => {
+    const cardHTML = `<div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" srcset="">
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul>
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following} <strong>Following</strong></li>
+                    <li>${user.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div id="repo">
+                    
+                </div>
+            </div>
+        </div>`
+        main.innerHTML = cardHTML;
+}  
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
