87b373de5a2f27e864347403d1bcbfcd775efa71
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-02-23 10:07:35 +0545

    function pointers

--- Untitled1.c
+++ Untitled1.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //macro
 #define pf printf
 
-int main(){
+int add (int x, int y){
+	return x+y;
+}
+int subtract (int x, int y){
+	return x-y;
+}
+void calc(int x, int y, int (*op)(int, int)){
+	printf("%d\n", op(x, y));
+}
+
+int compare (const void *a, const void *b){
+	return (*(int *)a)-(*(int *)b);
+}
+
+
+
+int main(){	
+	// Advanced C concepts
+	int xs[] = {1, 2, 3, 4, 5, 7, 6, 8, 10, 9}, i;
+	calc(10,5,add);
+	calc(10,5,subtract);
 	
-	printf("hello there");
-	pf("\nAbhyudaya");
+	qsort  (xs, 10, sizeof(int), compare);
+	for(i=0; i<10; i++){
+		printf("%d\t", xs[i]);
+	}
 	
 	return 0;
 }
