8d835f32f616858520110ace0ae7adbd9a4aacf7
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-03-07 14:01:49 +0545

    C

---
+++ C programming/learnig.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct person{
+    int roll_no;
+    char name[20];
+} p;
+
+int main(){
+
+    printf("Enter name and roll no of a student");
+    scanf("%s", p.name); scanf("%d", &p.roll_no);
+
+    printf("/nName: %s\t Roll No: %d", p.name, p.roll_no);
+    return 0;
+}
