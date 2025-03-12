4ca361292188d54fa93f55c90751e05bd7d352a9
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-03-12 14:42:53 +0545

    differences

--- learn some concepts/arrayloops.js
+++ learn some concepts/arrayloops.js
@@ -1,5 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 
-for (const num in arr) {
-    console.log(num);
+for (const keys in arr) {
+    console.log(keys);       // output: 0,1,2,3,4
 }
+
+arr.forEach((val)=>{
+    console.log(val);    // output: 1,2,3,4,5
+})
+
+// for in interates over keys which forEach iterates over values
