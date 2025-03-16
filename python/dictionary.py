1d5ab1e3bf58c42a5a3886094d5a188f22ccfbb5
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-03-16 15:27:49 +0545

    dkjfh

--- python/dictionary.py
+++ python/dictionary.py
@@ -1,7 +1,11 @@
 houses = {'abhyduaya': 'red', 'harry': 'blue'}
+people = ['a', 'b', 'c'];
 
 for person in houses:
     print(person)
 
 for person in houses:
     print(houses.get(person)) 
+
+for i in people:
+    print(i)
